ebb801d3f47368dbaff0a3c18122c81aca2e2259
Nami <havenothorn@gmail.com>
2024-02-04 18:51:11 +0900

    add: throttle

---
+++ react-deep-dive/throttling.js
@@ -0,0 +1,28 @@
+function throttle(func, limit) {
+  let lastFunc;
+  let lastRan;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(function () {
+        if (Date.now() - lastRan >= limit) {
+          func.apply(context, args);
+          lastRan = Date.now();
+        }
+      }, limit - (Date.now() - lastRan));
+    }
+  };
+}
+
+function handleEvent() {
+  console.log("Throttled Function Executed");
+}
+
+const throttledFunction = throttle(handleEvent, 2000);
